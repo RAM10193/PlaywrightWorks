32c32cff471c5d78eee5a29ea6226692624a5091
Ramkumar k <gkmram@gmail.com>
2023-06-26 11:20:10 +0100

    Playwright POC

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -11,11 +11,15 @@ test("Login Test Demo", async () => {
     //To launch a new tab in a browser
     const page = await context.newPage(); 
 
-    //To launch ORANGE HRM WEBSITE
-    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-    await page.fill("//input[@name='username']","Admin");
-    await page.fill("//input[@name='password']","admin123");
-    await page.click("//button[@type='submit']");
+    //To launch Ecomm WEBSITE
+    await page.goto("https://ecommerce-playground.lambdatest.io/");
+    await page.hover("//a[@data-toggle='dropdown']//span[contains(.,' My account')]");
+    await page.click("text=Login");
+
+    await page.fill("//input[@name='email']","gkmram@gmail.com");
+    await page.fill("//input[@name='password']","Ramkrishna93$");
+    await page.click("//input[@type='submit']");
+    
 
     await page.waitForTimeout(5000);
 
