ed0e4bc4017d892172afb44f2c884e7eadfdc39c
Ramkumar k <gkmram@gmail.com>
2023-06-26 10:37:37 +0100

    Playwright POC

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -3,7 +3,9 @@ import { chromium, test } from "@playwright/test";
 //An ASYNC fn.
 test("Login Test Demo", async () => {
     //To launch a Chromium based browser (Chrome, Edge, etc)
-    const browser = await chromium.launch();
+    const browser = await chromium.launch({
+        headless: false
+    });
     //To set a context for a page
     const context = await browser.newContext();
     //To launch a new tab in a browser
