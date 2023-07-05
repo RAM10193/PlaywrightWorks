bb3c7f8de33392b8c28ca5793a4d884956e8345f
Ramkumar k <gkmram@gmail.com>
2023-07-05 18:46:10 +0100

    visualcomparison

--- tests/visualcomparison.test.ts
+++ tests/visualcomparison.test.ts
@@ -3,6 +3,7 @@
 import { test, expect } from '@playwright/test';
 
 test('example test', async ({ page }) => {
+  //await page.goto('https://playwright.dev');
   await page.goto('https://ecommerce-playground.lambdatest.io/');
   await expect(page).toHaveScreenshot("landingpage.png");
   expect(await page.screenshot()).toMatchSnapshot("landingpage.png");
