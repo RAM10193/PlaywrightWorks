909e2248f9c94486baf6e993ebb7c71a371c41a0
Ramkumar k <gkmram@gmail.com>
2023-08-11 10:49:39 +0100

    update

--- tests/visualcomparison.test.ts
+++ tests/visualcomparison.test.ts
@@ -3,14 +3,14 @@
 import { test, expect } from '@playwright/test';
 import percySnapshot from '@percy/playwright';
 
-test('Login Test', async ({ page }) => {
+/*test('Login Test', async ({ page }) => {
   await page.goto('https://ecommerce-playground.lambdatest.io/index.php?route=account/login');
   const lgnbtn = page.locator("//input[@type='submit']");
   const valuelgn = await lgnbtn.getAttribute('value');
   console.log("Login button value is: "+valuelgn);  
   await expect(page).toHaveScreenshot("Login.png");
   expect(await page.screenshot()).toMatchSnapshot("Login.png");
-});
+});*/
 
 
 /*test('Submit Test', async ({ page }) => {
@@ -33,13 +33,13 @@ test('Login Test', async ({ page }) => {
   expect(await page.screenshot()).toMatchSnapshot("Login.png");
 });*/
 
-/*test('Percy Login Test', async ({ page }) => {
+test('Percy Login Test', async ({ page }) => {
   await page.goto('https://ecommerce-playground.lambdatest.io/index.php?route=account/login');
   const lgnbtn = page.locator("//input[@type='submit']");
   const valuelgn = await lgnbtn.getAttribute('value');
   console.log("Login button value is: "+valuelgn);  
   await percySnapshot(page,"Login page launched successfully");
-});*/
+});
 
 /*test('Percy Submit Test', async ({ page }) => {
   //test('Percy Submit Test', async ({ page }) => {
